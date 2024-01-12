c50fbae8c8c46d3b07c007be9db20cdb94413e69
Andrew-Sams <152635248+Andrew-Sams@users.noreply.github.com>
2024-01-12 18:34:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from streamlit_aggrid import AgGrid
 from scipy.stats import truncnorm
 
 # Function to generate asymmetrical Gaussian distribution
